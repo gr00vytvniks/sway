43dcd1da12bda27f22e398f3f7fb82bb8782e88f
Andrew Cann <shum@canndrew.org>
2022-04-28 18:41:46 +0800

    Disallow assigning to fields of non-mutable structs

--- sway-core/src/semantic_analysis/ast_node/mod.rs
+++ sway-core/src/semantic_analysis/ast_node/mod.rs
@@ -1018,6 +1018,33 @@ fn reassignment(
 
                         match expr {
                             Expression::VariableExpression { name, .. } => {
+                                match namespace.clone().get_symbol(&name).value {
+                                    Some(TypedDeclaration::VariableDeclaration(
+                                        TypedVariableDeclaration { is_mutable, .. },
+                                    )) => {
+                                        if !is_mutable.is_mutable() {
+                                            errors.push(CompileError::AssignmentToNonMutable(
+                                                name.as_str().to_string(),
+                                                name.span().clone(),
+                                            ));
+                                        }
+                                    }
+                                    Some(other) => {
+                                        errors.push(CompileError::ReassignmentToNonVariable {
+                                            name: name.clone(),
+                                            kind: other.friendly_name(),
+                                            span,
+                                        });
+                                        return err(warnings, errors);
+                                    }
+                                    None => {
+                                        errors.push(CompileError::UnknownVariable {
+                                            var_name: name.as_str().to_string(),
+                                            span: name.span().clone(),
+                                        });
+                                        return err(warnings, errors);
+                                    }
+                                }
                                 names_vec.push(ReassignmentLhs {
                                     name,
                                     r#type: type_checked.return_type,
