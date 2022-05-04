4e22c25fa7ec099792e2a5f29e6df2a2e196eabc
mitchmindtree <mitchell.nordine@fuel.sh>
2022-05-04 21:43:21 +1000

    Rename `temp_root` bindings to `scoped_root`
    This better represents the behaviour where the new `root` instance is
    scoped to the particular context (e.g. function body, code block, etc)
    and hence has no need to update the outer body.

--- sway-core/src/semantic_analysis/ast_node/declaration/function.rs
+++ sway-core/src/semantic_analysis/ast_node/declaration/function.rs
@@ -101,12 +101,12 @@ impl TypedFunctionDeclaration {
         let type_mapping = insert_type_parameters(&type_parameters);
 
         // insert parameters and generic type declarations into namespace
-        let mut temp_root = root.clone();
+        let mut scoped_root = root.clone();
 
         // check to see if the type parameters shadow one another
         for type_parameter in type_parameters.iter() {
             check!(
-                temp_root[mod_path]
+                scoped_root[mod_path]
                     .insert_symbol(type_parameter.name_ident.clone(), type_parameter.into()),
                 continue,
                 warnings,
@@ -119,7 +119,7 @@ impl TypedFunctionDeclaration {
                 match look_up_type_id(parameter.type_id).matches_type_parameter(&type_mapping) {
                     Some(matching_id) => insert_type(TypeInfo::Ref(matching_id)),
                     None => check!(
-                        temp_root.resolve_type_with_self(
+                        scoped_root.resolve_type_with_self(
                             mod_path,
                             look_up_type_id(parameter.type_id),
                             self_type,
@@ -133,7 +133,7 @@ impl TypedFunctionDeclaration {
                 };
         });
 
-        let namespace = &mut temp_root[mod_path];
+        let namespace = &mut scoped_root[mod_path];
         for FunctionParameter { name, type_id, .. } in parameters.clone() {
             namespace.insert_symbol(
                 name.clone(),
@@ -155,7 +155,7 @@ impl TypedFunctionDeclaration {
         let return_type = match return_type.matches_type_parameter(&type_mapping) {
             Some(matching_id) => insert_type(TypeInfo::Ref(matching_id)),
             None => check!(
-                temp_root.resolve_type_with_self(
+                scoped_root.resolve_type_with_self(
                     mod_path,
                     return_type,
                     self_type,
@@ -174,7 +174,7 @@ impl TypedFunctionDeclaration {
             TypedCodeBlock::type_check(TypeCheckArguments {
                 checkee: body.clone(),
                 init,
-                root: &mut temp_root,
+                root: &mut scoped_root,
                 mod_path,
                 return_type_annotation: return_type,
                 help_text:
